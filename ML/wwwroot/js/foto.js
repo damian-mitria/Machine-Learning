c5f5d8cada1e7957e28cd4878c91710a7a51a1b9
TomiiVaz <eliseovazquez01@gmail.com>
2022-11-07 21:11:39 -0300

    mas features

--- ML/wwwroot/js/foto.js
+++ ML/wwwroot/js/foto.js
@@ -2,9 +2,24 @@
 var resultado = document.getElementById("resultado")
 var porcent = document.getElementById("porcent").value
 var porcentaje = document.getElementById("porcentaje")
-//var file = document.getElementById("file").value
+var file = document.getElementById("upload-photo").value
+var error = document.getElementById("comprobadorF")
+var aceptado = document.getElementById("comprobadorT")
 
 
+function comprobar() {
+    if (file != '') {
+        error.className = "visible"
+        aceptado.className = "oculto"
+    } else {
+        aceptado.className = "visible"
+        error.className = "oculto"
+    }
+}
+
+document.getElementById('upload-photo').onchange = function () {
+    comprobar()
+};
 
 resultado.innerHTML = result;
 porcentaje.innerHTML = porcent;
